66498117b298622cb6790ea9e9e548ee78018ee7
olabudzyn <ola.budz96@gmail.com>
2019-10-26 14:19:33 +0200

    basic repository added v2

---
+++ src/main/java/com/budzynska/repository/UserRepo.java
@@ -0,0 +1,9 @@
+package com.budzynska.repository;
+
+import com.budzynska.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+}
